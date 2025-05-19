6866c4293cdbef1e40ff3ea2d62e3bdcc653ae6b
Stanéo644 <aurele.josserand@gmail.com>
2025-05-19 18:37:20 +0200

    add cross origin

--- api/sinatra/app.rb
+++ api/sinatra/app.rb
@@ -9,6 +9,23 @@ require './controllers/user_tags'
 require './controllers/search'
 require './controllers/websocket'
 
+require 'sinatra'
+require 'sinatra/cross_origin'
+
+configure do
+  enable :cross_origin
+end
+
+options '*' do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, X-Requested-With'
+  200
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
 
 
 get '/' do
